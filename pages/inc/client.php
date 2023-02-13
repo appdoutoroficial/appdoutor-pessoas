2847dba4c3c23c57b8005d35c3520365bed7a8b4
arthurappdoutor <99187536+arthurappdoutor@users.noreply.github.com>
2023-02-13 15:27:15 -0300

    Ajustes, padornização SCSS e Container.

---
+++ pages/inc/client.php
@@ -0,0 +1,35 @@
+<?php
+    function cabecalho(){
+        ?>
+            <!DOCTYPE html>
+                <html lang="pt-br">
+
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>App-Doutor</title>
+                    <link rel="stylesheet" href="../../assets/css/style.css">
+                    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+                    <script src="https://kit.fontawesome.com/0afbe024e0.js" crossorigin="anonymous"></script>
+                </head>
+
+                <body>
+                    <nav class="navbar navbar-expand-lg">
+                        <i class="fa-solid fa-chevron-left"><a href="#"></a></i>
+                        <div class="logo1">
+                            <img src="../../assets/images/logo-branco.png" class="logo" alt="">
+                        </div>
+                    </nav>
+        <?php
+    }
+
+    function footer(){
+        ?>
+                <script src="../../assets/js/plugins/jquery-3.3.1.min.js"></script>
+                <script src="../../assets/js/plugins/jquery.mask.js"></script>
+            </body>
+
+            </html>
+        <?php
+    }
