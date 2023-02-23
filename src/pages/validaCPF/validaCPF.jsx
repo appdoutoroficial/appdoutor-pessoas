38e8232a5c0e526452b71b0cd5badb69f3ce49d7
Jose Paiva <josepaiva@MacBook-Air-Jos.local>
2023-02-22 23:00:15 -0300

    Austes, SCSS, Axios, Login rename, Valida CPF:

--- src/pages/validaCPF/validaCPF.jsx
+++ src/pages/validaCPF/validaCPF.jsx
@@ -1,9 +1,25 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from '../../components/NavBar';
 import InputMask from 'react-input-mask';
+import axiosConfig from '../../axiosConfig';
 
+const ValidaCPF = () => {
+    const [cpf, setCpf] = useState();
 
-const validaCPF = () => {
+    useEffect(() => {
+        // Atualiza o título do documento usando a API do browser
+        document.title = `Você clicou 1 vezes`;
+    });
+
+    
+    const validaCPF = (elem) => {
+        axiosConfig.get("/Especialidades/Listar")
+        .then((response) => {
+            console.log(response);
+            // setCep(response.data);
+        })
+    }
+
     return (
         <>
             <Navbar />
@@ -20,7 +36,7 @@ const validaCPF = () => {
                 </div>
 
                 <div className="campo-cad">
-                    <InputMask mask="999.999.999-99" maskChar=" " />
+                    <InputMask mask="999.999.999-99" onBlur={validaCPF} name="cpf" className="cpf" maskChar=" " />
                     {/* <input type="text" className="cad-2" data-mask="000.000.000-00" name="cpf" id="cpf" placeholder="Digite seu CPF" aria-label="Recipiente para nickname" aria-describedby="basic-addon2" /> */}
                 </div>
 
@@ -30,4 +46,4 @@ const validaCPF = () => {
     )
 }
 
-export default validaCPF;
+export default ValidaCPF;
